373df4ebb67b6c3a82dc686fbe5d73ddc86df4d4
Cedricium <amaya.cedric@gmail.com>
2019-05-31 22:28:27 -0700

    Fix spacing in styled-component CSS

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 const HeaderWrapper = styled.div`
-display: flex;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   flex-direction: row;
